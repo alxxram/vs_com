4a4fae915a0bf9d253dfd64d953d18733902533a
alxxram <alexr@skycatch.com>
2015-02-17 18:17:33 -0800

    v8.0

--- vs_com.cpp
+++ vs_com.cpp
@@ -50,7 +50,7 @@ int VSCom::ParseArgs(int argc, char **argv)
         }
 
         if (vm.count("version")) {
-            cerr << "v7.0" << endl;
+            cerr << "v8.0" << endl;
             exit(0);
         }
 
